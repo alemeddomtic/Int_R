20f7e31083fe7de4b61ac5819227bcb45b94f0bf
Alexander Medina <143725975+alemeddomtic@users.noreply.github.com>
2024-06-26 17:07:47 -0600

    Add files via upload

---
+++ holamundo.R
@@ -0,0 +1,16 @@
+myString = "Hola mundo"
+print(myString)
+
+a.1=3
+a.2= 4
+a.3=5
+
+ if (FALSE){
+   "uno, dos, tres, cuatro, cinco, seis, siete
+   , ocho, nueve"
+ } 
+
+print(a.1)
+cat("a.1 es ", a.1, "\n")
+cat("a.2 es ", a.2, "\n")
+cat("a.3 es ", a.3, "\n")
