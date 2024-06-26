93a978c18eade4239f8a71a94c6ca127e4827eba
Alexander Medina <143725975+alemeddomtic@users.noreply.github.com>
2024-06-26 17:10:15 -0600

    Add files via upload

---
+++ EstrucruraDatos.R
@@ -0,0 +1,12 @@
+#vector
+# funcion c, combine
+manzana <- c('rojo', 'verde', "amarillo")
+print(manzana)
+
+#listado
+list1 <- list(c(1,2,3), 21.3, sin)
+print(list1)
+
+#matriz
+M = matrix(c("a", "b", "c", "d", "e", "a"), nrow= 2, ncol=3, byrow=FALSE)
+print(M)
---
+++ TiposDatos.R
@@ -0,0 +1,28 @@
+# tipos de datos
+
+#logico
+v = TRUE
+print(v)
+print(class(v))
+
+#numerico
+v= 256.3
+print(v)
+print(class(v))
+
+#entero
+v = 239L
+print(v)
+print(class(v))
+
+#complejo
+v= 234 + 5i
+print(class(v))
+
+#Caracter
+v= "Helo"
+print(class(v))
+
+#raw
+v= charToRaw("Hola")
+print(v)
